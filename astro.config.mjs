0de6a660d5dfd2e6451e56643600d496d95e1c3d
Lucas <lucashattry@gmail.com>
2024-11-03 22:53:57 +0100

    hebergement net

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://itweenzi.github.io.',
-    base: 'portfolio',
+  output: 'server',
+  adapter: netlify(),
+  
 });
